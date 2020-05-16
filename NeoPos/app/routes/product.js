8b8f3f852cdf42c3ff8eabfd63b0b9bfe2c202d3
mauric <mauric.caceres@gmail.com>
2020-05-16 13:51:13 -0300

    fix router productAdd

--- NeoPos/app/routes/product.js
+++ NeoPos/app/routes/product.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 
 /* GET product detail page. Home (product.ejs) */
-router.get('/:id', function(req, res, next) {
+router.get('/detalle/:id', function(req, res, next) {
     res.render('product', { title: 'Detalle del Producto' });
 });
 
-/* GET product add page. Home (index.html) */
+/* GET product add page. Home (productAdd.ejs) */
 router.get('/add', function(req, res, next) {
     res.render('productAdd', { title: 'Creacion del Producto' });
 });
