6f931b3d11eac93927e19a3b77fe26838559ea2f
JesusNicolasManrique <manrique.jesus.nicolas@gmail.com>
2020-05-17 19:18:14 -0300

    pagina de productos agregada con botones facha

--- NeoPos/app/app.js
+++ NeoPos/app/app.js
@@ -24,6 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/user', userRouter);
 app.use('/product', productRouter);
--- NeoPos/app/routes/product.js
+++ NeoPos/app/routes/product.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
+let productController = require("../controller/productController");
 
 
 /* GET product detail page. Home (product.ejs) */
-router.get('/detail/:id', function(req, res, next) {
+/* router.get('/detail/:id', function(req, res, next) {
     res.render('product', { title: 'Detalle del Producto' });
-});
+}); */
+
+/* GET product detail page. Home (product.ejs) */
+router.get('/detail/:id', productController.detail);
+
 
 /* GET product add page. Home (index.html) */
 router.get('/new', function(req, res, next) {
