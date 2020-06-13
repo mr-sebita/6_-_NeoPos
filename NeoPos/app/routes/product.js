8d3a24455b9cbee77d2e1446ec6258179b6f326a
mauric <mauric_rck@hotmail.com>
2020-06-12 22:28:04 -0300

    jesus changes merges

--- NeoPos/app/routes/product.js
+++ NeoPos/app/routes/product.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require('express');
 var router = express.Router();
 let productController = require("../controller/productController");
@@ -24,23 +23,3 @@ router.post('/create', productController.createProduct);
 module.exports = router;
 
 
-=======
-var express = require('express');
-var router = express.Router();
-let productController = require("../controller/productController");
-
-
-/* GET product detail page. Home (product.ejs) */
-/* router.get('/detail/:id', function(req, res, next) {
-    res.render('product', { title: 'Detalle del Producto' });
-}); */
-
-/* GET product detail page. Home (product.ejs) */
-router.get('/detail/:id', productController.detail);
-
-
-/* GET product add page. Home (index.html) */
-router.get('/new', productController.new);
-
-module.exports = router;
->>>>>>> origin/Jesu3.0
