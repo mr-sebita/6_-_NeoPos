6b840de75f0662315c2a89ed340cb91086d8b42f
martinchoaguilera <martinale93aguilera@gmail.com>
2020-07-27 13:28:34 -0300

    asd

--- public/javascripts/validation.js
+++ public/javascripts/validation.js
@@ -1,3 +1,9 @@
 window.onload= () => {
     //MAÑANA VOY A VALIDAR FORMULARIO DESDE EL FRONT (MARTIN - SABADO 25/7 )
+    // alert('hola');
+    let formulario = document.getElementById( 'signup' );
+
+    formulario.addEventListener( 'submit' , (event) =>{
+            
+    })
 }
--- routes/shop.js
+++ routes/shop.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 let shopController = require("../controller/shopController");
 
-router.get('/',shopMiddlewares,shopController.indexdb);
+router.get('/',shopController.index);
 
 module.exports = router;
