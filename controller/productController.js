8589eb72334e316afc965475f437c6b35ad3b554
mauric <mauric_rck@hotmail.com>
2020-07-29 08:06:14 -0300

    code useless removed

--- controller/productController.js
+++ controller/productController.js
@@ -8,38 +8,36 @@ const db = require('../database/models');
 * @private
 */
 let productController = {
-    archivo: path.join(__dirname, '/../models/' + 'product.json'),
     detail: (req, res, next) => {
         let productById = searchById(req.params.id);
         if (productById != null) {
-            res.render('product', { data: productById, user:req.session.user });
+            res.render('product', { data: productById, user: req.session.user });
         } else {
             res.render('productNotExist', { data: req.protocol + '://' + req.get('host') + req.originalUrl });
         }
-    },    
+    },
     detaildb: (req, res, next) => {
         db.Product.findByPk(req.params.id)
             .then(function (resultados) {
                 if (resultados != undefined) {
-                    res.render('product', {data: resultados, admin: req.session.admin});
+                    res.render('product', { data: resultados, admin: req.session.admin });
                 } else {
                     res.render('productNotExist', { data: req.protocol + '://' + req.get('host') + req.originalUrl });
                 }
             });
-        },
-
+    },
     newProduct: (req, res, next) => {
         res.render('productNew', { title: 'Creacion del Producto' });
     },
     createProduct: (req, res) => {
         console.log("creando producto");
         db.Product.create({
-            img         : req.body.img,
-            price       : req.body.price.trim(),
-            title       : req.body.title.trim(),
-            brand       : req.body.brand.trim(),
-            description : req.body.description.trim(),
-            categoria   : req.body.categoria.trim()
+            img: req.body.img,
+            price: req.body.price.trim(),
+            title: req.body.title.trim(),
+            brand: req.body.brand.trim(),
+            description: req.body.description.trim(),
+            categoria: req.body.categoria.trim()
         });
 
         res.redirect('shop');
@@ -68,11 +66,11 @@ let productController = {
         // }
         //let product = searchById(req.params.id); //conocemos el producto a editar
         //console.log(product);
-        
+
         // console.log('El usuario logueado es : ' + req.session.usuarioLogueado.name);
 
         //if (product != null) {
-            res.render('productEdit', { data: product });
+        res.render('productEdit', { data: product });
         //} else
         //    res.render('productNotExist', { data: req.protocol + '://' + req.get('host') + req.originalUrl });
 
@@ -81,24 +79,26 @@ let productController = {
 
         db.Product.update(
             // Set Attribute values 
-                  { title       : req.body.name,
-                    price       : req.body.price,
-                    img         : req.body.img,
-                    description : req.body.description,
-                    discount    : req.bod.discount },
+            {
+                title: req.body.name,
+                price: req.body.price,
+                img: req.body.img,
+                description: req.body.description,
+                discount: req.bod.discount
+            },
             // Where clause / criteria 
-                   { _id : req.params.id }
-           ).success(function() { 
-          
-               console.log("Project updated successfully!");
-          
-           }).error(function(err) { 
-          
-               console.log("Project update failed !");
-               //handle error here
-          
-           });
-           res.redirect('/shop');
+            { _id: req.params.id }
+        ).success(function () {
+
+            console.log("Project updated successfully!");
+
+        }).error(function (err) {
+
+            console.log("Project update failed !");
+            //handle error here
+
+        });
+        res.redirect('/shop');
         // let product = searchById(req.params.id); //conocemos el producto a editar
         // let products = readJson(); // traemos el json de productos, parseado con la función
         // console.log(product);
