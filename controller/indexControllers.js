f7c970c470899e0e1760e639f80f36913dd49e31
martinchoaguilera <martinale93aguilera@gmail.com>
2020-08-10 00:29:26 -0300

    s

--- controller/indexControllers.js
+++ controller/indexControllers.js
@@ -2,7 +2,8 @@ const db = require('../database/models');
 
 let indexController = {
     index: async ( req , res , next  ) => {
-        let shops = await db.Shop.findAll() 
+        let shops = await db.Shop.findAll()
+        console.log(shops) 
         res.render( 'index' ,{ user : req.session.user , shops: shops });
     }
 }
